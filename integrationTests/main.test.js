04533497b5dc858f41ce7bb1436b00870b9417ca
Vijay Joshua Nadar <32163394+vjnadar@users.noreply.github.com>
2020-03-09 20:34:21 +0530

    minor correction.

--- integrationTests/main.test.js
+++ integrationTests/main.test.js
@@ -8,7 +8,7 @@ describe("main route test", () => {
     server.close();
   });
   describe("test", () => {
-    it("should response coming from /main/test", async () => {
+    it("should check the response coming from /main/test", async () => {
       let res = await request(server).get("/main/test");
       expect(res.status).toBe(200);
     });
